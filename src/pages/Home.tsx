ccb9d738ad21705451df03d446c46ed04c6aa8ae
numfor13 <numforprecious7@gmail.com>
2026-03-01 19:06:50 +0100

    correcting picture import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,23 +8,39 @@ import { validateCameroonPhone } from "../utils/validators";
 import { Search, Star } from "lucide-react";
 import { motion } from "framer-motion";
 
+import cosmetics from "../assets/images/cosmetics.jpg";
+import accessories from "../assets/images/accessories.jpg";
+import education from "../assets/images/education.webp";
+import finance from "../assets/images/finance.jpg";
+import food from "../assets/images/food.jpg";
+import hair from "../assets/images/hair.jpg";
+import services from "../assets/images/services.jpg";
+import travel from "../assets/images/travel.jpg";
+import Arts from "../assets/images/Arts.jpg";
+import others from "../assets/images/others.png";
+import electronics from "../assets/images/electronics.jpg"
+import Clothes from "../assets/images/Clothes.jpg"
+import shoes from "../assets/images/shoes.jpeg"
+import skin_care from "../assets/images/skin_care.jpg"
+import health from "../assets/images/health.png"
+
 // Placeholder data
 const categories = [
-    {name: "Electronics", img: "src/assets/images/electronics.jpg"},
-    { name: "Fashion" , img: "src/assets/images/Clothes-rails.jpg" },
-    {name: "Shoes", img: "src/assets/images/shoes.jpeg"},
-    {name: "Skin Care", img: "src/assets/images/Face-and-Body-Products-RidgelysRadar.com_-scaled.jpg"},
-    {name: "Health and Wellness", img: "src/assets/images/Best-Health-And-Well-Being-Training-In-Dubai-746x512.png"},
-    {name: "Cosmetics", img: "src/assets/images/cosmetics.jpg"},
-    {name: "Accessories", img: "src/assets/images/accessories.jpg"},
-    {name: "Education", img: "src/assets/images/education.webp"},
-    {name: "Finance", img: "src/assets/images/finance.jpg"},
-    {name: "Food and Beverages", img: "src/assets/images/food and beverages.jpg"},
-    {name: "Hair Suppies", img: "src/assets/images/hair supplies.jpg"},
-    {name: "Services", img: "src/assets/images/services.jpg"},
-    {name: "Travel", img: "src/assets/images/travel.jpg"},
-    {name: "Arts and Entertainment", img: "src/assets/images/Arts-Entertainment-2.jpg"},
-    {name: "Others", img: "src/assets/images/others.png"},
+    {name: "Electronics", img: electronics},
+    { name: "Fashion" , img: Clothes },
+    {name: "Shoes", img: shoes},
+    {name: "Skin Care", img: skin_care},
+    {name: "Health and Wellness", img: health},
+    {name: "Cosmetics", img: cosmetics},
+    {name: "Accessories", img: accessories},
+    {name: "Education", img: education},
+    {name: "Finance", img: finance},
+    {name: "Food and Beverages", img: food},
+    {name: "Hair Suppies", img: hair},
+    {name: "Services", img: services},
+    {name: "Travel", img: travel},
+    {name: "Arts and Entertainment", img: Arts},
+    {name: "Others", img: others},
   ];
 
   const popular = [
